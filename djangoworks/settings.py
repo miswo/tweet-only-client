231cc3b8df69cd9ae0e1fae55cd733f76174edd7
Miswo <ichigoichie09@gmail.com>
2018-10-08 21:12:50 +1100

    image2

--- djangoworks/settings.py
+++ djangoworks/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
@@ -147,7 +149,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
-STATIC_ROOT = 'staticfiles'
+#STATIC_ROOT = 'staticfiles'
 
 DEBUG = False
 
@@ -156,3 +158,4 @@ try:
 except ImportError:
     pass
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
